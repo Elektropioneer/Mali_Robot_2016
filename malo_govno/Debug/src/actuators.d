50ead2a9cc1350c7d5c37492ca7dbc43341a825e
MrBurnzie <gproduct1379@yahoo.com>
2016-04-05 00:38:35 +0200

    added umbrella functions (shouldnt be conflicts)

--- malo_govno/Debug/src/actuators.d
+++ malo_govno/Debug/src/actuators.d
@@ -1,56 +1,50 @@
-src/actuators.d src/actuators.o: ../src/actuators.c \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocan128.h \
- D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocanxx.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
- ../inc/system.h ../inc/can.h ../inc/actuators.h
+src/actuators.d src/actuators.o src/actuators.o: ../src/actuators.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocan128.h \
+  D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocanxx.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  ../inc/system.h ../inc/can.h ../inc/actuators.h
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
 
 D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocan128.h:
 
 D:\Atmel\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iocanxx.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
 
 ../inc/system.h:
 
--- malo_govno/Debug/src/odometry.d
+++ malo_govno/Debug/src/odometry.d
@@ -1,36 +1,32 @@
-src/odometry.d src/odometry.o: ../src/odometry.c \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- ../inc/odometry.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdbool.h \
- ../inc/system.h ../inc/can.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+src/odometry.d src/odometry.o src/odometry.o: ../src/odometry.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h ../inc/odometry.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdbool.h \
+  ../inc/system.h ../inc/can.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/math.h
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
 
 ../inc/odometry.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdbool.h:
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdbool.h:
 
 ../inc/system.h:
 
 ../inc/can.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
 
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/math.h:
